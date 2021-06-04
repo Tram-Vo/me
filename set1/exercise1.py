4b9616096a65688787de28e617e4d9cab2ad4f87
Tram Vo <email@example.com>
2021-06-04 16:04:09 +1000

    week 1

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -1,9 +1,1 @@
-"""Your very first python program!
-
-TODO: write a python script that prints "Hello world!"
-
-Look on the course website for how to do this, and for how to run the tests.
-
-https://design-computing.github.io/md/week1
-
-"""
+print("Hello world!")
